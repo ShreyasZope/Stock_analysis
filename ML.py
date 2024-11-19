a132615983699a696ba8258e5df6ddd59893773b
ShreyasZope <shreyaszope123@gmail.com>
2024-11-19 23:05:19 +0530

    first commit

---
+++ ML.py
@@ -0,0 +1,92 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+
+
+stock_symbols = {
+    "Apple": "AAPL",
+    "Alphabet (Google)": "GOOGL",
+    "Amazon": "AMZN",
+    "Microsoft": "MSFT",
+    "Tesla": "TSLA",
+    "NVIDIA": "NVDA",
+    "Meta Platforms": "META",
+    "JPMorgan Chase": "JPM",
+    "Visa": "V",
+    "Mastercard": "MA",
+    "PayPal": "PYPL",
+    "Bank of America": "BAC",
+    "Walmart": "WMT",
+    "Disney": "DIS",
+    "Johnson & Johnson": "JNJ",
+    "Procter & Gamble": "PG",
+    "UnitedHealth Group": "UNH",
+    "Exxon Mobil": "XOM",
+    "Chevron": "CVX",
+    "Berkshire Hathaway": "BRK-B",
+    "Eli Lilly": "LLY",
+    "Abbott Laboratories": "ABT",
+    "Merck": "MRK",
+    "Pfizer": "PFE",
+    "Thermo Fisher Scientific": "TMO",
+    "Home Depot": "HD",
+    "Lowe's": "LOW",
+    "Costco": "COST",
+    "McDonald's": "MCD",
+    "Starbucks": "SBUX",
+}
+while True:
+    try:
+        stock_check=input()
+        ticker = yf.Ticker(stock_symbols[stock_check])
+        hist = ticker.history(start="2024-11-14", end="2024-11-15",interval="15m")
+
+        # Print the first 5 rows
+        print(hist)
+        print("Do you want to end!!!")
+        B=input()
+        if B=="yes":
+            break
+    except KeyError:
+        print("Invalid stock symbol.")
+
+print("Enter Grapstyle you want \n \t 1)Simple Line Plot \n \t 2)Candlestick Chart\n \t 3)Volume Chart\n \t 4)Combined Chart")
+Choise=int(input())
+if Choise==1:
+    plt.figure(figsize=(12, 6))
+    plt.plot(hist.index, hist['Close'])
+    plt.xlabel('Date')
+    plt.ylabel('Closing Price')
+    plt.title('Closing Price of AAPL')
+    plt.grid(True)
+    plt.show()
+elif Choise==2:
+    mpf.plot(hist, type='candle', style='yahoo', title='AAPL Candlestick Chart', ylabel='Price')
+elif Choise==3:
+    plt.figure(figsize=(12, 6))
+    plt.bar(hist.index, hist['Volume'], color='gray')
+    plt.xlabel('Date')
+    plt.ylabel('Volume')
+    plt.title('Trading Volume of AAPL')
+    plt.grid(True)
+    plt.show()
+elif Choise==4:
+    fig, ax1 = plt.subplots(figsize=(12, 6))
+
+    color = 'tab:blue'
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Price', color=color)
+    ax1.plot(hist.index, hist['Close'], color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+
+    color = 'tab:red'
+    ax2.set_ylabel('Volume', color=color)
+    ax2.bar(hist.index, hist['Volume'], color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    plt.show()
